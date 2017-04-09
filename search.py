25e5a533c757284d342710407f4995d2709f38e4
Ahmed Yehia <ahmed.yehia311@gmail.com>
2017-04-10 01:57:44 +0200

    [INIT] Add exact match and book_id option for search, Also add option to set directory on starting server

--- search.py
+++ search.py
@@ -5,21 +5,49 @@ from flask import g
 
 from epubsearch import EpubIndexer
 from epubsearch import crossdomain
+from optparse import OptionParser
+import os
 
 app = Flask(__name__)
-index = EpubIndexer("whoosh")
-
-@app.route("/")
-def home():
-    return "try /search?q=whale"
 
 @app.route("/search", methods=['GET','OPTIONS'])
 @crossdomain(origin='*')
+
 def search():
-    query = request.args.get('q')
-    results = index.search(query)
-    return jsonify(**results)
+    global container_path
+    query       = request.args.get('q')
+    book_id     = request.args.get('id')
+    exact_match = request.args.get('exact_match')
+    path_prefix = container_path
+    index_path  = os.path.join(path_prefix, book_id, 'search_index')
+
+    if(os.path.isdir(index_path) and len(query) >= 3):
+        index   = EpubIndexer("whoosh", index_path)
+        results = index.search(query, exact_match=exact_match)
+        return jsonify(**results)
+
+    return jsonify(results=[])
+
+def flaskrun(app):
+    """
+    Takes a flask.Flask instance and runs it. Parses command-line flags to configure the app.
+    """
+    parser = OptionParser()
+    parser.add_option('-p', '--path',help='Path to the container folder that contains epub unzipped folders', dest='container_path')
+    parser.add_option('-d', '--debug',help="set to True to enable debug, Dont enable debug in production", dest='debug')
+    (options, args) = parser.parse_args()
+
+    if not options.container_path:
+        options.container_path = os.getcwd()
+
+    if not options.debug:
+        options.debug=False
+
+    # set path in global varaiable so i can access it in the search method
+    global container_path
+    container_path = options.container_path
+    # Make sure debug is set to false in production
+    app.run(debug=options.debug)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    flaskrun(app)
